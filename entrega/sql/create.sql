3dcdeb39c1751e51510efff271b27aa5685f9668
elderstoffel <elderstoffel@gmail.com>
2023-03-03 22:26:38 -0300

    proxy reverso com bd e php

---
+++ entrega/sql/create.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS meubanco;
+use meubanco;
+
+CREATE TABLE IF NOT EXISTS dados (
+    AlunoID int,
+    Nome varchar(50),
+    Sobrenome varchar(50),
+    Endereco varchar(150),
+    Cidade varchar(50),
+    Host varchar(50)
+);
